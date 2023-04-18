7836b56da27f330401a6a2cf62f90173ef345314
xHyroM <generalkubo@gmail.com>
2023-04-18 17:51:55 +0200

    refactor: rebrand

--- src/main/java/dev/inditium/ExampleMod.java
+++ src/main/java/dev/inditium/ExampleMod.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example;
+package dev.inditium;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
--- src/main/java/dev/inditium/mixin/ExampleMixin.java
+++ src/main/java/dev/inditium/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package dev.inditium.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import dev.inditium.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
