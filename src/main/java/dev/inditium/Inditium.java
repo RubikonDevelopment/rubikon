58de354e96d843b47d22ecdfc32f723cef9c7420
xHyroM <generalkubo@gmail.com>
2023-04-20 15:13:12 +0200

    fix: remove peace of trash

--- src/main/java/dev/inditium/Inditium.java
+++ src/main/java/dev/inditium/Inditium.java
@@ -1,14 +1,9 @@
 package dev.inditium;
 
 import dev.inditium.managers.ModuleManager;
-import dev.inditium.renderer.nanvog.core.NVContext;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import org.lwjgl.nanovg.NVGColor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static org.lwjgl.nanovg.NanoVG.*;
 
 public class Inditium implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -24,29 +19,5 @@ public class Inditium implements ModInitializer {
 		// Proceed with mild caution.
 
 		MODULE_MANAGER.init();
-
-		HudRenderCallback.EVENT.register((e,l) -> {
-			NVContext.draw((ctx) ->{
-				nvgBeginPath(ctx);
-				nvgRect(ctx, 10, 10, 100, 100);
-				nvgFillColor(ctx, nvgColor(-1));
-				nvgFill(ctx);
-				nvgClosePath(ctx);
-			});
-		});
-	}
-
-	public static NVGColor nvgColor(int argb) {
-		NVGColor _res = NVGColor.create();
-		nvgRGBA((byte) (argb >> 16 & 255), (byte) (argb >> 8 & 255), (byte) (argb >> 0 & 255), (byte) (argb >> 24 & 255), _res);
-		return _res;
-	}
-
-	public static NVGColor rgba(int r, int g, int b, int a, NVGColor color) {
-		color.r(r / 255.0F);
-		color.g(g / 255.0F);
-		color.b(b / 255.0F);
-		color.a(a / 255.0F);
-		return color;
 	}
 }
