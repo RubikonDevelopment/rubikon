c81d1fd5d69552f36655e2e54657be94da8dcb0d
xHyroM <generalkubo@gmail.com>
2023-05-01 17:26:10 +0200

    docs: update serializable java-docs

--- src/main/java/dev/rubikon/utils/Serializable.java
+++ src/main/java/dev/rubikon/utils/Serializable.java
@@ -4,7 +4,8 @@ package dev.rubikon.utils;
 import net.minecraft.nbt.NbtCompound;
 
 /**
- * This interface is used to save and load data from the config(json) file.
+ * This interface is used to serialize and deserialize objects.
+ * @param <T> The type of the object to be serialized.
  */
 public interface Serializable<T> {
     NbtCompound toNbtTag();
